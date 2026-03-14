a7ed1df3c2fd680a08d6903154f5a30364067697
Prathmesh Nikam <Prathmesh.Nikam@creditsafe.com>
2026-03-15 01:38:36 +0530

    Added error handling while making api call

--- phase1-ai-foundations/summarizer.py
+++ phase1-ai-foundations/summarizer.py
@@ -7,13 +7,16 @@ load_dotenv()
 client = genai.Client()
 
 def main():
-    text = input("Enter the text to summarize: ")
-    response = client.models.generate_content(
-        model="gemini-3.1-flash-lite-preview", 
-        contents=f"Summarize the following text in 3 concise bullet points.:\n\n{text}"
-    )
-    summary = response.text
-    print(f"\nSummary:\n{summary}")
+    try:
+        text = input("Enter the text to summarize: ")
+        response = client.models.generate_content(
+            model="gemini-3.1-flash-lite-preview", 
+            contents=f"Summarize the following text in 3 concise bullet points.:\n\n{text}"
+        )
+        summary = response.text
+        print(f"\nSummary:\n{summary}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
