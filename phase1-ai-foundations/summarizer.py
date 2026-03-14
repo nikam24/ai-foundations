66bd0a0889a049b22b20d5d5d139cdfb787aa2c1
Prathmesh Nikam <Prathmesh.Nikam@creditsafe.com>
2026-03-15 01:31:52 +0530

    Text summarizer using google gemini-ai api

---
+++ phase1-ai-foundations/summarizer.py
@@ -0,0 +1,20 @@
+import os
+from urllib import response 
+from dotenv import load_dotenv 
+from google import genai
+
+load_dotenv() 
+
+client = genai.Client()
+
+def main():
+    text = input("Enter the text to summarize: ")
+    response = client.models.generate_content(
+        model="gemini-3.1-flash-lite-preview", 
+        contents=f"Summarize the following text:\n\n{text}"
+    )
+    summary = response.text
+    print(f"\nSummary:\n{summary}")
+
+if __name__ == "__main__":
+    main()
